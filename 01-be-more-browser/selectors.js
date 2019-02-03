fba1eba0b97b5afe1f0dba51ac21dd0c3781c583
chenga@wix.com <chenga@wix.com>
2019-02-03 22:18:54 +0200

    Initial commit

---
+++ 01-be-more-browser/selectors.js
@@ -0,0 +1,6 @@
+document.querySelector("div").innerText = "this selects a div"
+document.querySelector("#div").innerText = "this selects what?"
+document.querySelector("#password").value = "1337"
+document.querySelector("div>[type=checkbox]").checked=true
+document.querySelector("div.social>*").innerText = facebook
+document.querySelector("div.social>*").innerText = "facebook"
