4a8048179991cb0720652931436ac8317759fd25
chenga@wix.com <chenga@wix.com>
2019-02-20 14:41:34 +0200

    chalk

---
+++ chalk.js
@@ -0,0 +1,8 @@
+/**
+ * Make this scrip work!
+ * Start with npm init
+ * Then npm install –save <name of the module you need>
+ * Take a look at the package.json created
+ */
+const chalk = require('chalk');
+console.log(chalk.blue('Hello world!'));
