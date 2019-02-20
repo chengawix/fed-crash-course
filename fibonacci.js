f4c1b729af94e8082b81f1b5df21fb7c33818b30
chenga@wix.com <chenga@wix.com>
2019-02-20 13:09:43 +0200

    fibo

---
+++ fibonacci.js
@@ -0,0 +1,11 @@
+/*
+Build a function that prints a Fibonacci sequence:
+F(0) = 0
+F(1) = 1
+F(N) = F(n-1) + F(n-2)
+Make sure you know where stop!
+
+Tips: 
+Run this script with: node fibonacci.js
+Make sure you call your function
+*/
